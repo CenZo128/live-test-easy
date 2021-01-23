8a18b44fe1736fd508560796b685bbaed8bdfffd
Vincent Guizot <vincentguizot128@gmail.com>
2021-01-23 14:19:00 +0700

    soal

---
+++ 1.js
@@ -0,0 +1,37 @@
+/**
+ * ARLOJI COLLECTION
+ * 
+ * A punya koleksi arloji yang dia banggakan, dan teman-teman nya ingin bertanya koleksi arloji apa saja yang dia punya.
+ * 
+ * let watches = ["Swiss Army","Daniel Wellington","Dublot"]
+ * Hasil yang di harapkan : Saya punya Swiss Army, Daniel Wellington, dan Dublot.
+ * 
+ * Hasil harus sama persis seperti yang di contohkan, dimana :
+ * 1. Result di print dalam 1 string 
+ * 2. Tanda ',' (koma) dan '.' (titik) harus sesuai
+ * 
+ * Contoh lain :
+ * let arloji = ['A', 'B', 'C', 'D','E']
+ * Hasil yang di harapkan : Saya punya A, B, C, D, dan E.
+ * 
+ * BUAT PSEUDOCODE DAN SOLUSI CODINGNYA
+ * 
+ * Rules : Tidak boleh menggunakan built in function apapun!
+ */
+
+function mentionWatch(watches) {
+  // your code here
+
+}
+
+console.log(mentionWatch(["Swiss Army", "Daniel Wellington", "Dublot"])) 
+// Saya punya Swiss Army, Daniel Wellington, dan Dublot.
+console.log(mentionWatch(["A", "B", "C", "D", "E"]))
+// Saya punya A, B, C, D, dan E.
+console.log(mentionWatch([]))
+// Saya tidak punya arloji, trims.
+console.log(mentionWatch())
+// Saya tidak punya arloji, trims.
+
+// abaikan kode di bawah ini
+module.exports = mentionWatch
