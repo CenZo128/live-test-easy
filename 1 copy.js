54fc44755fb56941b9673667ba44f4fb1b2649e0
Vincent Guizot <vincentguizot128@gmail.com>
2021-02-07 12:43:16 +0700

    soal baru

---
+++ 1 copy.js
@@ -0,0 +1,33 @@
+/**
+ * ==========
+ * Pseudocode
+ * ==========
+ * Buatlah pseudocode untuk situasi berikut:
+ * 
+ * Ada sebuah bola salju berguling menuruni bukit.
+ * Kecepatan bola salju tersebut adalah 3m/s (3 meter per detik)
+ * Jika bola salju menabrak batu, maka kecepatannya akan berkurang
+ * - menabrak batu kecil, kecepatan berkurang 0.5m/s
+ * - menabrak batu sedang, kecepatan berkurang 1.5m/s
+ * - menabrak batu besar, bola salju berhenti.
+ * 
+ * Di bukit tersebut terdapat banyak batu,
+ * ada x batu kecil, y batu sedang, dan z batu besar.
+ * 
+ * Cetaklah kecepatan akhir bola salju tersebut.
+ * 
+ * Note: kecepatan bola salju tidak bisa minus
+ * (sudah jatuh dari atas bukit tidak bisa naik lagi sendiri)
+ * 
+ * Contoh kasus #1
+ * Input => Terdapat 1 buah Batu kecil
+ * Output => Kecepatan akhir bola salu adalah 2.5m/s
+ * 
+ * Contoh kasus #2
+ * Input => Terdapat 1 buah batu besar
+ * output => ketemu batu besar, maka bola salju berhenti
+ * 
+ * Contoh kasus #3
+ * Input => Terdapat 1 buah batu kecil, 2 buah batu sedang
+ * output => Kecepatan akhir bola salu adalah 0m/s
+ */
